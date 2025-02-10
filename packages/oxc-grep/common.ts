aab0ea328af548d26b3bab169b56c60edea5525e
Ally <9024018+AllySummers@users.noreply.github.com>
2025-02-11 06:11:24 +1100

    Fix

--- packages/oxc-grep/common.ts
+++ packages/oxc-grep/common.ts
@@ -1,54 +1,54 @@
 import type { ParseResult } from 'npm:oxc-parser@0.48.2';
 
 export const EXTENSIONS = [
-	'js',
-	'mjs',
-	'cjs',
-	'jsx',
-	'ts',
-	'tsx',
-	'mts',
+    'js',
+    'mjs',
+    'cjs',
+    'jsx',
+    'ts',
+    'tsx',
+    'mts',
 ];
 
 export interface ContextOptions {
-	before: number;
-	after: number;
+    before: number;
+    after: number;
 }
 
 export interface ParsedMatch {
-	filename: string;
-	line: number;
-	column: number;
-	content: string;
+    filename: string;
+    line: number;
+    column: number;
+    content: string;
 }
 
 export interface ReadASTResult {
-	content: string;
-	ast: ParseResult;
+    content: string;
+    ast: ParseResult;
 }
 
 export interface ExpandGlobsOptions {
-	paths: string[];
-	root: string;
-	exclude?: string[];
+    paths: string[];
+    root: string;
+    exclude?: string[];
 }
 
 export interface ExpandGlobsOutput {
-	files: string[];
+    files: string[];
 }
 
 export type WorkerEventInput = FindASTMatchesOptions & ExpandGlobsOptions;
 
 export interface FindASTMatchesOptions {
-	file: string;
-	patterns: string[];
-	context: ContextOptions;
-	root: string;
+    file: string;
+    patterns: string[];
+    context: ContextOptions;
+    root: string;
 }
 
 export interface FindASTMatchesOutput {
-	matches: ParsedMatch[];
+    matches: ParsedMatch[];
 }
 export type WorkerEventOutput =
-	| Partial<FindASTMatchesOutput & ExpandGlobsOutput>
-	| undefined;
+    | Partial<FindASTMatchesOutput & ExpandGlobsOutput>
+    | undefined;
--- packages/oxc-grep/mod.ts
+++ packages/oxc-grep/mod.ts
@@ -2,16 +2,16 @@ import meow from 'npm:meow@13.2.0';
 import { FixedThreadPool } from 'jsr:@poolifier/poolifier-web-worker@0.4.31';
 import { pooledMap } from 'jsr:@std/async@1.0.10/pool';
 import type {
-	ExpandGlobsOptions,
-	ExpandGlobsOutput,
-	FindASTMatchesOptions,
-	FindASTMatchesOutput,
+    ExpandGlobsOptions,
+    ExpandGlobsOutput,
+    FindASTMatchesOptions,
+    FindASTMatchesOutput,
 } from './common.ts';
 
 const scriptName = 'oxc-grep';
 
 const cli = meow(
-	`
+    `
 	OXC Grep
 
 	Usage:
@@ -27,133 +27,133 @@ const cli = meow(
 		-d <path>, --dir <path>                  Directory to search in (default: cwd)
 		-h, --help                               Show this help
 	`,
-	{
-		importMeta: import.meta,
-		argv: Deno.args,
-		pkg: {
-			name: scriptName,
-			version: '0.0.1',
-		},
-		flags: {
-			pattern: {
-				type: 'string',
-				shortFlag: 'p',
-				isMultiple: true,
-			},
-			exclude: {
-				type: 'string',
-				shortFlag: 'e',
-				isMultiple: true,
-			},
-			before: {
-				type: 'number',
-				shortFlag: 'B',
-			},
-			after: {
-				type: 'number',
-				shortFlag: 'A',
-			},
-			context: {
-				type: 'number',
-				shortFlag: 'C',
-			},
-			concurrency: {
-				type: 'number',
-				shortFlag: 'c',
-				default: navigator.hardwareConcurrency,
-			},
-			dir: {
-				type: 'string',
-				shortFlag: 'd',
-				default: Deno.cwd(),
-			},
-		},
-	},
+    {
+        importMeta: import.meta,
+        argv: Deno.args,
+        pkg: {
+            name: scriptName,
+            version: '0.0.1',
+        },
+        flags: {
+            pattern: {
+                type: 'string',
+                shortFlag: 'p',
+                isMultiple: true,
+            },
+            exclude: {
+                type: 'string',
+                shortFlag: 'e',
+                isMultiple: true,
+            },
+            before: {
+                type: 'number',
+                shortFlag: 'B',
+            },
+            after: {
+                type: 'number',
+                shortFlag: 'A',
+            },
+            context: {
+                type: 'number',
+                shortFlag: 'C',
+            },
+            concurrency: {
+                type: 'number',
+                shortFlag: 'c',
+                default: navigator.hardwareConcurrency,
+            },
+            dir: {
+                type: 'string',
+                shortFlag: 'd',
+                default: Deno.cwd(),
+            },
+        },
+    },
 );
 
 const getCLIArgs = (args: typeof cli) => {
-	const [pattern, ...paths] = args.input;
-	const {
-		exclude = [],
-		pattern: patterns = [],
-		before,
-		after,
-		context,
-		concurrency,
-		dir,
-	} = args.flags;
+    const [pattern, ...paths] = args.input;
+    const {
+        exclude = [],
+        pattern: patterns = [],
+        before,
+        after,
+        context,
+        concurrency,
+        dir,
+    } = args.flags;
 
-	return {
-		patterns: [pattern, ...patterns],
-		paths: paths.length ? paths : ['./'],
-		exclude,
-		context: {
-			before: Math.max(before ?? 0, context ?? 0),
-			after: Math.max(after ?? 0, context ?? 0),
-		},
-		concurrency,
-		dir,
-	};
+    return {
+        patterns: [pattern, ...patterns],
+        paths: paths.length ? paths : ['./'],
+        exclude,
+        context: {
+            before: Math.max(before ?? 0, context ?? 0),
+            after: Math.max(after ?? 0, context ?? 0),
+        },
+        concurrency,
+        dir,
+    };
 };
 
 const { patterns, paths, exclude, context, concurrency, dir } = getCLIArgs(
-	cli,
+    cli,
 );
 
 const pool = new FixedThreadPool<
-	FindASTMatchesOptions | ExpandGlobsOptions,
-	FindASTMatchesOutput | ExpandGlobsOutput
+    FindASTMatchesOptions | ExpandGlobsOptions,
+    FindASTMatchesOutput | ExpandGlobsOutput
 >(
-	concurrency,
-	new URL('./worker.ts', import.meta.url),
-	{ errorEventHandler: console.error },
+    concurrency,
+    new URL('./worker.ts', import.meta.url),
+    { errorEventHandler: console.error },
 );
 
 if (!pool.info.started) {
-	pool.start();
+    pool.start();
 }
 
 try {
-	for await (
-		const globOutput of pooledMap(
-			concurrency,
-			paths,
-			(path) =>
-				pool.execute({
-					paths: [path],
-					exclude,
-					root: dir,
-				}, 'expandGlobs'),
-		)
-	) {
-		if (!('files' in globOutput)) {
-			continue;
-		}
+    for await (
+        const globOutput of pooledMap(
+            concurrency,
+            paths,
+            (path) =>
+                pool.execute({
+                    paths: [path],
+                    exclude,
+                    root: dir,
+                }, 'expandGlobs'),
+        )
+    ) {
+        if (!('files' in globOutput)) {
+            continue;
+        }
 
-		const { files } = globOutput;
+        const { files } = globOutput;
 
-		for await (
-			const matchesOutput of pooledMap(
-				concurrency,
-				files,
-				(file) =>
-					pool.execute(
-						{ file, patterns, context, root: dir },
-						'findASTMatches',
-					),
-			)
-		) {
-			if (!('matches' in matchesOutput)) {
-				continue;
-			}
+        for await (
+            const matchesOutput of pooledMap(
+                concurrency,
+                files,
+                (file) =>
+                    pool.execute(
+                        { file, patterns, context, root: dir },
+                        'findASTMatches',
+                    ),
+            )
+        ) {
+            if (!('matches' in matchesOutput)) {
+                continue;
+            }
 
-			for (const match of matchesOutput.matches) {
-				console.log(match.content + `\n`);
-			}
-		}
-	}
+            for (const match of matchesOutput.matches) {
+                console.log(match.content + `\n`);
+            }
+        }
+    }
 } catch (error) {
-	console.error(error);
+    console.error(error);
 } finally {
-	pool.destroy();
+    pool.destroy();
 }
